f0a97d539459faa148325f71ec65a9ee0258baa4
Oleh Yanivskyy <oleh_01@mail.ru>
2017-05-11 02:04:29 +0300

    Modify main

--- src/main/java/com/cw/recommendation/system/main/Runner.java
+++ src/main/java/com/cw/recommendation/system/main/Runner.java
@@ -3,6 +3,8 @@ package com.cw.recommendation.system.main;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.cw.recommendation.system.config.AppConfig;
+import com.cw.recommendation.system.entity.Module;
+import com.cw.recommendation.system.entity.Subject;
 import com.cw.recommendation.system.service.application.CommonRecommendationService;
 import com.cw.recommendation.system.service.application.SubjectSelectorService;
 
@@ -17,6 +19,13 @@ public class Runner {
 
 		System.out.println(recommendationService.recommendSubjects(1L, 1L, minSubjects));
 		System.out.println(recommendationService.recommendSubjects(1L, 1L, minYears));
+
+		for (Subject subject : recommendationService.recommendSubjects(1L, 1L, minYears)) {
+			System.out.println(subject.getName() + ": ");
+
+			for (Module module : subject.getSubjectModules())
+				System.out.println("  - " + module.getName());
+		}
 		ctx.close();
 	}
 
